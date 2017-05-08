632cc8595a79e771db4b9e9014323daa4080f891
Adura Israel <johndoe@example.com>
2017-05-08 15:30:32 +0100

    first commit

---
+++ js/untitled.js
@@ -0,0 +1,24 @@
+//global variable
+var myColors = ["white","black","blue","red","yellow"];
+
+
+
+//question
+(function(){
+console.log("My favorite color is " + myColors[0]);
+console.log("My second best color is " + myColors[1]);
+console.log("Followed by " + myColors[2]);
+console.log("then " + myColors[3]);
+console.log("And finally " + myColors[4]);
+})();
+
+
+
+	//Bonus question.
+	(function(){
+console.log("My 1st choice:" + myColors[0]);
+console.log("My 2nd choice:" + myColors[1]);
+console.log("My 3rd choice:" + myColors[2]);
+console.log("My 4th choice:" + myColors[3]);
+console.log("My 5th choice:" + myColors[4]);
+})();
